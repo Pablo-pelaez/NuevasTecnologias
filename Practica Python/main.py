71ba0bfc9eb5f0da0e0bafdec09132c5d8f0f427
DanielOcampo2402 <danielocampo020816@gmail.com>
2021-09-03 21:00:07 -0500

    Taller terminado

--- Practica Python/main.py
+++ Practica Python/main.py
@@ -3,4 +3,4 @@ from Vehiculo import Vehiculo
 
 ObjVehiculo = Vehiculo("DTF365", "Wolswagen", "2019", 100000000)
 ObjVehiculo.setMarca("Renault Logan")
-print(f"La marca es: {ObjVehiculo.getMarca()}")
+print(f"La marca es: {ObjVehiculo.getMarca()}") 
