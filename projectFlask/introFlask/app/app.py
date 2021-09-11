f7a0f77fe6d40ad6e20c52a56ab63afc970a4dbf
defaultUser <71094683+Pablo-pelaez@users.noreply.github.com>
2021-09-10 21:04:41 -0500

    v2

--- projectFlask/introFlask/app/app.py
+++ projectFlask/introFlask/app/app.py
@@ -4,8 +4,21 @@ app = Flask(__name__)
 
 @app.route('/') 
 def index():
-    # return "<h1 style='color: steelblue' >Hello from Flask!</h1>"
-    return render_template('index.html')
+    productos = ['Mouse Inalambrico', 'Teclado', 'SSD 512gb', 'Diadema']
+    datos = {
+        'titulo': 'Sistema de facturacion',
+        'subtitulo': 'Manejo de clientes',
+        'productos': ['Mouse Inalambrico', 'Teclado', 'SSD 512gb', 'Diadema'],
+        'cantProductos': len(productos)
+    }
+
+    return render_template('index.html', data=datos)
+
+
+@app.route('/login')
+def login():
+    user = "Jdom"
+    return render_template('login.html', user = user)
 
 if __name__ == '__main__':
     app.run(debug=True, port= 3500)
